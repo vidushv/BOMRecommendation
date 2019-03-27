cbe3f1437e2c584d867ba45932db8e8acf95fd7c
Mukul Shukla <ms3528@cornell.edu>
2019-03-27 17:41:09 -0400

    service layer

--- src/main/java/com/cornell/se/bom/bomRecom/BomRecomApplication.java
+++ src/main/java/com/cornell/se/bom/bomRecom/BomRecomApplication.java
@@ -1,9 +1,12 @@
 package com.cornell.se.bom.bomRecom;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.cornell.se.bom.controller.CDPOSController;
@@ -12,6 +15,9 @@ import com.cornell.se.bom.controller.CDPOSController;
 @ComponentScan(basePackageClasses = CDPOSController.class)
 @EnableJpaRepositories("com.cornell.se.bom.repository")
 @EntityScan(basePackages = {"com.cornell.se.bom.model"})
+@EnableAutoConfiguration
+@Configuration
+@ImportResource({"classpath*:applicationContext.xml"})
 public class BomRecomApplication {
 
 	public static void main(String[] args) {
