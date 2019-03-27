71e8cb860d1c752cb93c8dc4783a4aed33350ccb
Mukul Shukla <ms3528@cornell.edu>
2019-03-26 21:48:16 -0400

    spring boot initial setup mysql hibernate

---
+++ src/main/java/com/cornell/se/bom/bomRecom/BomRecomApplication.java
@@ -0,0 +1,13 @@
+package com.cornell.se.bom.bomRecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomRecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomRecomApplication.class, args);
+	}
+
+}
