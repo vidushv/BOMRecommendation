7fc8b02d5bd2928e25c85513023394505fba05df
Vidush Vishwanath <vidushv@gmail.com>
2019-03-27 20:48:32 -0400

    csv_to_json function added

--- machine_learning/parseData.py
+++ machine_learning/parseData.py
@@ -1,5 +1,21 @@
 import pandas as pd 
+import csv 
+import json
 
 def readData(file_name):
     data = pd.read_csv(file_name)
     return data
+
+def write_data(file_name, data):
+    with open(file_name,'w') as resultFile:
+        wr = csv.writer(resultFile)
+        wr.writerows(data)
+
+def csv_to_json(csv_file, json_file):
+    csvfile = open(csv_file, 'r')
+    jsonfile = open(json_file, 'w')
+    fieldnames = ("ID","Val")
+    reader = csv.DictReader( csvfile, fieldnames)
+    for row in reader: 
+        json.dump(row, jsonfile)
+        jsonfile.write('\n')   
